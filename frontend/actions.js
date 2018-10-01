d219538bc6c62f31ec6d8e9bb350cee22e13bedc
Craig Kaiser <craig@kaiserengineering.io>
2018-10-01 11:16:29 -0400

    Move to promises

--- frontend/actions.js
+++ frontend/actions.js
@@ -13,15 +13,24 @@ export function changeMessageText(message) {
 export function onChangeAuthorText(author) {
     return { type: CHANGE_AUTHOR, author: author };
 }
+
 // Replace me with promises
 export function addMessage() {
     return (dispatch, getState) => {
-        $.ajax({
-            url: '/review',
-            type: 'POST',
-            contentType: 'application/json',
-            data: JSON.stringify({ message : getState().message, author : getState().author }),
-            success: () => dispatch({ type: ADD_REVIEW }),
-        });
+        return fetch('/review', {
+            method: "POST", // *GET, POST, PUT, DELETE, etc.
+            mode: "cors", // no-cors, cors, *same-origin
+            cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: "same-origin", // include, same-origin, *omit
+            headers: {
+                "Content-Type": "application/json; charset=utf-8",
+            },
+            redirect: "follow", // manual, *follow, error
+            referrer: "no-referrer", // no-referrer, *client
+            body: JSON.stringify({ message : getState().message, author : getState().author }), // body data type must match "Content-Type" header
+        })
+        .then(function(){
+          dispatch({ type: ADD_REVIEW });
+      });
     };
 }
