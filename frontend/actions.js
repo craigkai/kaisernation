9af5dd032169499d22c31588011de833e835085c
Craig Kaiser <craig@kaiserengineering.io>
2018-10-05 08:03:01 -0400

    Don't allow empty posts

--- frontend/actions.js
+++ frontend/actions.js
@@ -17,20 +17,22 @@ export function onChangeAuthorText(author) {
 // Replace me with promises
 export function addMessage() {
     return (dispatch, getState) => {
-        return fetch('/review', {
-            method: "POST", // *GET, POST, PUT, DELETE, etc.
-            mode: "cors", // no-cors, cors, *same-origin
-            cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-            credentials: "same-origin", // include, same-origin, *omit
-            headers: {
-                "Content-Type": "application/json; charset=utf-8",
-            },
-            redirect: "follow", // manual, *follow, error
-            referrer: "no-referrer", // no-referrer, *client
-            body: JSON.stringify({ message : getState().message, author : getState().author }), // body data type must match "Content-Type" header
-        })
-        .then(function(){
-          dispatch({ type: ADD_REVIEW });
-      });
+        if (getState().message && getState().author) {
+            return fetch('/review', {
+                method: "POST", // *GET, POST, PUT, DELETE, etc.
+                mode: "cors", // no-cors, cors, *same-origin
+                cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+                credentials: "same-origin", // include, same-origin, *omit
+                headers: {
+                    "Content-Type": "application/json; charset=utf-8",
+                },
+                redirect: "follow", // manual, *follow, error
+                referrer: "no-referrer", // no-referrer, *client
+                body: JSON.stringify({ message : getState().message, author : getState().author }), // body data type must match "Content-Type" header
+            })
+            .then(function(){
+            dispatch({ type: ADD_REVIEW });
+        });
+        }
     };
 }
